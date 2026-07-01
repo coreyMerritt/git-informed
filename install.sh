68fac70fe5a7c8f0d0b3b51f74e6f17027cb1799
Corey Merritt <coreyleemerritt@gmail.com>
2026-07-01 02:06:56 -0400

    Add python setup to install script

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ set -euo pipefail
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
-sudo ln -s "${script_dir}/start.sh" "/usr/bin/gitbs"
+# Execute
+python -m venv .venv
+./.venv/bin/pip install -e .[dev]
+if [[ ! -L "/usr/bin/gitbs" ]]; then
+  sudo ln -s "${script_dir}/start.sh" "/usr/bin/gitbs"
+fi
