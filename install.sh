a650cfd82400aaae745464f01efb909633e6d116
Corey Merritt <coreyleemerritt@gmail.com>
2026-07-02 02:12:18 -0400

    Add vars to install script

--- install.sh
+++ install.sh
@@ -3,10 +3,13 @@
 set -euo pipefail
 
 script_dir="$(dirname "$(readlink -f "$0")")"
+binary_dir="/usr/bin"
+binary_name="giti"
 
 # Execute
-python -m venv .venv
+rm -rf "./.venv"
+python -m venv "./.venv"
 ./.venv/bin/pip install -e .[dev]
-if [[ ! -L "/usr/bin/gitbs" ]]; then
-  sudo ln -s "${script_dir}/start.sh" "/usr/bin/gitbs"
+if [[ ! -L "${binary_dir}/${binary_name}" ]]; then
+  sudo ln -s "${script_dir}/start.sh" "${binary_dir}/${binary_name}"
 fi
